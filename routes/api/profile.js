5c7f1ad35be56838a6ad241b29451934b6b1625b
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-26 10:25:31 -0600

    added delete profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -263,6 +263,22 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', {
     }).catch(err => res.status(404).json(err));
 });
 
+//@route  DELETE /api/profile/
+//@desc   Delete user and profile
+//@access Private
+
+router.delete('/', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+
+      User.findOneAndRemove({ _id: req.user.id }).then(() => res.json({ success: true }));
+
+    }).catch(err => res.status(404).json(err));
+});
+
+
+
 
 
 module.exports = router;
