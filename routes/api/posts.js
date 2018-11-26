b1acd2a05b686516609d3ccc91b0276a93375cdc
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-26 11:42:48 -0600

    delete post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,10 +5,11 @@ const passport = require('passport');
 const validatePostInput = require('../../validation/post');
 
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 //@route  POST /api/posts
 //@desc   Create post
-//@access Public
+//@access Private
 router.post('/', passport.authenticate('jwt', {
   session: false
 }), (req, res) => {
@@ -39,7 +40,9 @@ router.get('/', (req, res) => {
   Post.find().sort({
       date: -1
     }).then(posts => res.json(posts))
-    .catch(err => res.status(404).json(err));
+    .catch(err => res.status(404).json({
+      noPost: 'No posts found'
+    }));
 });
 
 //@route  GET /api/posts/:id
@@ -47,8 +50,40 @@ router.get('/', (req, res) => {
 //@access Public
 router.get('/:id', (req, res) => {
   Post.findById(req.params.id)
-      .then(post => res.json(post))
-      .catch(err => res.status(404).json(err));
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({
+      noPost: 'No post found'
+    }));
+});
+
+//@route  DELETE /api/posts/:id
+//@desc   Delet a post
+//@access Private
+router.delete('/:id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  Profile.findOne({
+    user: req.user.id
+  }).then(profile => {
+
+    Post.findById(req.params.id).then(post => {
+
+      if(post.user.toString() !== req.user.id){
+
+        return res.status(401).json({
+          unauthorized: 'You are unauthorized to delete this post'
+        });
+      }
+
+      post.remove().then(() => res.json({ success: true }));
+
+    }).catch(err => res.status(404).json({
+      noPost: 'No post was found by the id: ' + req.user.id
+    }))
+
+  }).catch(err => res.status(404).json({
+    unauthorized: 'Could not find user'
+  }))
 });
 
 module.exports = router;
