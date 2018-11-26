7107f85b1b461ecb9864bda030fc8467085eaa37
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-25 23:19:45 -0600

    added bearer token on login

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-  mongoURI: 'mongodb://username:1username@ds115244.mlab.com:15244/dev-connector'
+  mongoURI: 'mongodb://username:1username@ds115244.mlab.com:15244/dev-connector',
+  secretOrKey: 'secret'
 
 }
