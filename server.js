b133d25dd885893965e67246318d06d3270c63b7
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-25 22:07:26 -0600

    added mongoose

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db).then(() => console.log('mongoose connected')).catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('test'));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on ${port}`));
+
