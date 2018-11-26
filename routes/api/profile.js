e2b3663c799075378affdf3604b448765856d1ca
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-26 10:13:25 -0600

    added delete route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -231,6 +231,22 @@ router.post('/education', passport.authenticate('jwt', {
     });
 });
 
+//@route  DELETE /api/profile/experience/:exp_id
+//@desc   Delete education to profile
+//@access Private
+
+router.delete('/experience/:exp_id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+      profile.experience.splice(removeIndex, 1);
+
+      profile.save().then(profile => res.json(profile));
+    }).catch(err => res.status(404).json(err));
+});
+
 
 
 module.exports = router;
