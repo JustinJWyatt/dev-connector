de8d4081b9620544be6352af609714062c4045cf
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-26 20:06:47 -0600

    added logout on navbar

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -37,3 +37,9 @@ export const setCurrentUser = decoded => {
     payload: decoded
   }
 }
+
+export const logoutUser = () => dispatch => {
+  localStorage.removeItem('jwtToken');
+  setAuthToken(false);
+  dispatch(setCurrentUser({}))
+}
