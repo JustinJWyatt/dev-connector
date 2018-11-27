56701a79a739d086f6a2b7f634f6136d661ed315
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-27 07:23:00 -0600

    added dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from './store';
+import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Dashboard from "./components/dashboard/Dashboard";
+
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
@@ -14,19 +16,18 @@ import Login from "./components/auth/Login";
 
 import "./App.css";
 
-if(localStorage.jwtToken){
+if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
   const decoded = jwt_decode(localStorage.jwtToken);
   store.dispatch(setCurrentUser(decoded));
   const now = Date.now() / 1000;
-  if(decoded.exp < now){
+  if (decoded.exp < now) {
     store.dispatch(logoutUser());
-    window.location.href = '/login';
+    window.location.href = "/login";
   }
 }
 
 class App extends Component {
-
   render() {
     return (
       <Provider store={store}>
@@ -37,6 +38,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
